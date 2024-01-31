c8a52693455fe724217a2610b0facd2a81726dad
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2024-01-31 12:26:49 +0100

    updated stochastic functions

--- ecopylot/stats.py
+++ ecopylot/stats.py
@@ -21,3 +21,49 @@ def _get_dataframe_stats(df: pd.DataFrame) -> dict:
 
 array = sarrays.UncertaintyBase.from_dicts(*[self.data[key] for key in keys])
         rng = sarrays.MCRandomNumberGenerator(array)
+
+def generate_stochastic_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds stochastic columns to the dataframe.
+
+    For single values, this would look like:
+
+    | UID (index) | parameter | ... | uncertainty_type | loc | min | max  |
+    |-------------|-----------|-----|------------------|-----|-----|------|
+    | 123         | foo       | ... | 1                | 0.1 | NaN | NaN  |
+    | 456         | bar       | ... | 2                | 8.4 | 2   | 11.2 |
+
+    if we now choose iterations = 3, we get:
+    
+    | UID (index) | parameter | ... | uncertainty_type | loc | min | max  | iteration | stochastic |
+    |-------------|-----------|-----|------------------|-----|-----|------|-----------|------------|
+    | 123         | foo       | ... | 1                | 0.1 | NaN | NaN  | 1         | 0.1        |
+    | 123         | foo       | ... | 1                | 0.1 | NaN | NaN  | 2         | 0.1        |
+    | 123         | foo       | ... | 1                | 0.1 | NaN | NaN  | 3         | 0.1        |
+    | 456         | bar       | ... | 2                | 8.4 | 2   | 11.2 | 1         | 9.2        |
+    | 456         | bar       | ... | 2                | 8.4 | 2   | 11.2 | 2         | 7.8        |
+    | 456         | bar       | ... | 2                | 8.4 | 2   | 11.2 | 3         | 8.9        |
+
+
+    ```
+    {
+        (...)
+        "uncertainty_type": 1, # static value (no uncertainty)
+        "loc": 0.1,
+        "min": NaN,
+        "max": NaN,
+        (...)
+    }
+    ```
+    For distributions, this would look like:
+    ```
+    {
+        (...)
+        "uncertainty_type": 2, # lognormal distribution
+        "loc": 8.4,
+        "min": 2,
+        "max": 11.2,
+        (...)
+    }
+    ```
+    """

fc063dc8934795aa8cdf321fb0fe0e11f5985e9b
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2024-02-06 08:16:49 +0100

    updated function draft

--- ecopylot/excel_to_json.py
+++ ecopylot/excel_to_json.py
@@ -38,32 +38,46 @@ df['new1'] = df['uncertainty distribution'].replace(config['uncertainty_distribu
 """
 How do I transform the following dataframe:
 
-| index | 2001 | 2001 | 2002 | 2002 | ... |
-|-------|------|------|------|------|-----|
-| 1     | low  | high | low  | high | ... |
-| 2     | 1    | 2    | 7    | 8    | ... |
-| 3     | NaN  | NaN  | 12   | 13   | ... |
-
-| index | 1    | 2    | 3    | 4    | ... |
-|-------|------|------|------|------|-----|
-| 1     | 2001 | 2001 | 2002 | 2002 | ... |
-| 2     | low  | high | low  | high | ... |
-| 3     | 1    | 2    | 7    | 8    | ... |
-| 4     | NaN  | NaN  | 12   | 13   | ... |
-
-|       | 2001        | 2002        | ... |
-| index | low  | high | low  | high | ... |
-|-------|------|------|------|------|-----|
-| 1     | 1    | 2    | 7    | 8    | ... |
-| 2     | NaN  | NaN  | 12   | 13   | ... |
-
+| index | 0         | 1    | 2    | 3    | 4    | ... |
+|-------|-----------|------|------|------|------|-----|
+| 0     | parameter | 2001 | 2001 | 2002 | 2002 | ... |
+| 1     |           | low  | high | low  | high | ... |
+| 2     | foo       | 1    | 2    | 7    | 8    | ... |
+| 3     | bar       | NaN  | NaN  | 12   | 13   | ... |
 
 
 to:
 
-| index | year | low | high | ... |
-|-------|------|-----|------|-----|
-| 1     | 2001 | 1   | 2    | ... |
-| 2     | 2002 | 7   | 8    | ... |
-| 3     | 2002 | 12  | 13   | ... |
+| index | parameter | year | low | high | ... |
+|-------|-----------|------|-----|------|-----|
+| 0     | foo       | 2001 | 1   | 2    | ... |
+| 1     | foo       | 2002 | 7   | 8    | ... |
+| 2     | bar       | 2002 | 12  | 13   | ... |
 """
+
+"""
+| index | 1    | 2    | 3    | 4    | ... |
+|-------|------|------|------|------|-----|
+| 0     | 2001 | 2001 | 2002 | 2002 | ... |
+| 1     | low  | high | low  | high | ... |
+| 2     | 1    | 2    | 7    | 8    | ... |
+| 3     | NaN  | NaN  | 12   | 13   | ... |
+
+|       | 2001        | 2002        | ... |
+| index | low  | high | low  | high | ... |
+|-------|------|------|------|------|-----|
+| 0     | 1    | 2    | 7    | 8    | ... |
+| 1     | NaN  | NaN  | 12   | 13   | ... |
+"""
+
+dfyears = df[[col for col in df.columns.unique() if isinstance(df.iloc[0][col], int)]]
+
+multiindex = pd.MultiIndex.from_product(
+    [
+        list(dfyears.iloc[0].unique()),
+        list(dfyears.iloc[1].unique())
+    ]
+)
+
+dfyears.columns = multiindex
+dfyears = dfyears.drop([0, 1])

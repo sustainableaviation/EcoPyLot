a8421bf2b6d8d1e8cd3181171f358c53851c91a4
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2024-02-14 08:32:07 +0100

    updated function

--- ecopylot/excel_to_json.py
+++ ecopylot/excel_to_json.py
@@ -15,10 +15,6 @@ mypath = Path("/Users/michaelweinold/github/EcoPyLot/dev/other/Input data_bus.xl
 
 # %%
 
-def myfunc(x):
-    return str([item.strip() for item in x.split(',')])
-
-
 def _load_excel(excel_input: pathlib.PurePath) -> pd.DataFrame:
     """
     Loads data from an Excel `xls` or `xlsx` file into a DataFrame.
@@ -71,14 +67,6 @@ def _uncertainty_distribution_string_to_code(
     df['uncertainty'] = df[uncertainty_col].replace(uncertainty_dict)
     return df
 
-# some testing
-
-df = _load_excel(mypath)
-
-# %%
-
-df['new1'] = df[9].replace(config['uncertainty_distributions_mapping'])
-# %%
 
 def _stack_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -102,7 +90,29 @@ def _stack_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     Inspired by: https://pandas.pydata.org/docs/user_guide/reshaping.html#stack-and-unstack
 
     """
-    df = df.set_index(0).stack().reset_index()
+
+    # move all string columns to the index
+    # this is to make sure the stack function works only on 'year' columns
+
+    list_cols: list = list(df.columns.get_level_values(0))
+    list_string_cols: list = [item for item in list_cols if isinstance(item, str)]
+    df = df.set_index(keys = list_string_cols)
+
+    df = df.stack(
+        level = 0, # stack 'year' columns
+        future_stack=True # https://pandas.pydata.org/docs/whatsnew/v2.1.0.html#new-implementation-of-dataframe-stack
+    )
+
+    df = df.dropna(
+        axis = 0,
+        how = 'all'
+    )
+
+    # move the string columns back from the index
+    # and reset the index
+
+    df = df.reset_index(drop = False)
+
     return df
 
 
@@ -144,74 +154,6 @@ def _set_dataframe_indices(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-# to do: years to 
-
-"""
-How do I transform the following dataframe:
-
-| index | 0         | 1    | 2    | 3    | 4    | ... |
-|-------|-----------|------|------|------|------|-----|
-| 0     | parameter | 2001 | 2001 | 2002 | 2002 | ... |
-| 1     |           | low  | high | low  | high | ... |
-| 2     | foo       | 1    | 2    | 7    | 8    | ... |
-| 3     | bar       | NaN  | NaN  | 12   | 13   | ... |
-
-
-to:
-
-| index | parameter | year | low | high | ... |
-|-------|-----------|------|-----|------|-----|
-| 0     | foo       | 2001 | 1   | 2    | ... |
-| 1     | foo       | 2002 | 7   | 8    | ... |
-| 2     | bar       | 2002 | 12  | 13   | ... |
-"""
-
-"""
-| index | 1    | 2    | 3    | 4    | ... |
-|-------|------|------|------|------|-----|
-| 0     | 2001 | 2001 | 2002 | 2002 | ... |
-| 1     | low  | high | low  | high | ... |
-| 2     | 1    | 2    | 7    | 8    | ... |
-| 3     | NaN  | NaN  | 12   | 13   | ... |
-
-|       | 2001        | 2002        | ... |
-| index | low  | high | low  | high | ... |
-|-------|------|------|------|------|-----|
-| 0     | 1    | 2    | 7    | 8    | ... |
-| 1     | NaN  | NaN  | 12   | 13   | ... |
-"""
-
-dfyears = df[[col for col in df.columns.unique() if isinstance(df.iloc[0][col], int)]]
-
-multiindex = pd.MultiIndex.from_product(
-    [
-        list(dfyears.iloc[0].unique()),
-        list(dfyears.iloc[1].unique())
-    ]
-)
-
-dfyears.columns = multiindex
-dfyears = dfyears.drop([0, 1])
-
-# %%
-
-data = {
-    0: ['parameter', '', 'foo', 'bar'],
-    1: [2001, 'low', 1, np.nan],
-    2: [2001, 'high', 2, np.nan],
-    3: [2002, 'low', 7, 12],
-    4: [2002, 'high', 8, 13],
-}
-
-df = pd.DataFrame(data)
-
-idx = pd.MultiIndex.from_arrays([df.iloc[0, 1:], df.iloc[1, 1:]],
-                                names=('year', None))
-
-out = df.iloc[2:].set_index(0).set_axis(idx, axis=1).rename_axis('parameter').stack(0).reset_index()
-
-# https://stackoverflow.com/a/77945979/7331016
-
 def load_data_from_excel(excel_input: pathlib.PurePath) -> pd.DataFrame:
     """
     Loads data from an Excel `xls` or `xlsx` file into a DataFrame.
@@ -222,7 +164,7 @@ def load_data_from_excel(excel_input: pathlib.PurePath) -> pd.DataFrame:
     
     | index | A         | B              | C                  | D    | E    | F    | G    | H    | I    | ... |
     |-------|-----------|----------------|--------------------|------|------|------|------|------|------|-----|
-    | 1     | parameter | classification | uncertainty distr. | 2001 | 2021 | 2001 | 2002 | 2002 | 2002 | ... |
+    | 1     | parameter | classification | uncertainty distr. | 2001 | 2001 | 2001 | 2002 | 2002 | 2002 | ... |
     | 2     |           |                |                    | loc  | low  | high | loc  | low  | high | ... |
     | 3     | foo       | alpha, beta    | triangular         | 1.5  | 1    | 2    | 8    | 7    | 8.5  | ... |
     | 4     | bar       | gamma, delta   | triangular         |      |      |      | 14   | 12   | 17   | ... |
@@ -240,4 +182,44 @@ def load_data_from_excel(excel_input: pathlib.PurePath) -> pd.DataFrame:
     https://stats-arrays.readthedocs.io/en/latest/index.html#mapping-parameter-array-columns-to-uncertainty-distributions
 
     """
+    pass
+
 # %%
+
+# some testing
+
+dfex = _load_excel(mypath)
+
+
+# %%
+data = {
+    0: ['parameter', '', 'foo', 'bar'],
+    1: ['classification', '', 'alpha, beta', 'gamma, delta'],
+    2: [2001, 'low', 1, np.nan],
+    3: [2001, 'high', 2, np.nan],
+    4: [2002, 'low', 7, 12],
+    5: [2002, 'high', 8, 13],
+}
+
+df = pd.DataFrame(data)
+
+# %%
+
+dfyears = df[[col for col in df.columns.unique() if isinstance(df.iloc[0][col], int)]]
+
+multiindex = pd.MultiIndex.from_product(
+    [
+        list(dfyears.iloc[0].unique()),
+        list(dfyears.iloc[1].unique())
+    ]
+)
+
+dfyears.columns = multiindex
+dfyears = dfyears.drop([0, 1])
+
+idx = pd.MultiIndex.from_arrays([df.iloc[0, 1:], df.iloc[1, 1:]],
+                                names=('year', None))
+
+out = df.iloc[2:].set_index(0).set_axis(idx, axis=1).rename_axis('parameter').stack(0).reset_index()
+
+# https://stackoverflow.com/a/77945979/7331016
